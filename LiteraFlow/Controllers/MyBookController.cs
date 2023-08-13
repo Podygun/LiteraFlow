31f579b68bb643dfb9e4d1ae466c0a2f10ca9af4
Pody <vladpodyganov@yandex.ru>
2023-08-13 16:56:46 +0300

    Add my book controller

---
+++ LiteraFlow/Controllers/MyBookController.cs
@@ -0,0 +1,39 @@
+﻿]using LiteraFlow.Web.BL.Books;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiteraFlow.Web.Controllers;
+
+public class MyBookController : Controller
+{
+    private readonly IBooks books;
+
+    public MyBookController(IBooks books)
+    {
+        this.books = books;
+    }
+
+    [HttpGet]
+    [Route("/mybook")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("/mybook")]
+    public IActionResult PostBook(BookViewModel book)
+    {
+        if (!ModelState.IsValid)
+        {
+            return Redirect("/mybook");
+        }
+        return View();
+    }
+
+    [HttpPost]
+    [Route("/mybook/chapter")]
+    public IActionResult PostChapter(ChapterViewModel chapter)
+    {
+        return View();
+    }
+}
