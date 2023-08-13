7f4e2b1d525990142ddce05f650a49c48975ed3c
Pody <vladpodyganov@yandex.ru>
2023-08-13 16:57:04 +0300

    Book and chapter View Models

---
+++ LiteraFlow/ViewModels/BookViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace LiteraFlow.Web.ViewModels;
+
+public class BookViewModel
+{
+    [Required]
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string? Title { get; set; } = null!;
+
+    [Required]
+    public int TypeId { get; set; }
+
+    [Required]
+    public int GenreId { get; set; }
+
+    [Required]
+    public int StatusId { get; set; } = 1;
+
+    [MaxLength(500)]
+    public string? Note { get; set; }
+
+    [MaxLength(500)]
+    public string? Description { get; set; }
+
+
+    public bool IsAdultContent { get; set; } = false;
+
+
+    public int WhoCanWatch { get; set; } = 1;
+    public int WhoCanDownload { get; set; } = 1;
+    public int WhoCanComment { get; set; } = 1;
+
+    public int? AmountUnlockedChapters { get; set; } = null;
+    public string? BookImage { get; set; } = null;
+    public double? Price { get; set; } = null;
+}
---
+++ LiteraFlow/ViewModels/ChapterViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace LiteraFlow.Web.ViewModels;
+
+public class ChapterViewModel
+{
+    [Required]
+    [MinLength(1)]
+    [MaxLength(64)]
+    public string Title { get; set; } = null!;
+
+    [Required]
+    [MinLength(1)]
+    [MaxLength(250_000)]
+    public string Text { get; set; } = null!;
+}
