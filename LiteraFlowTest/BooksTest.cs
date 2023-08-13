cd39194b9fcbec672e3bb8d682468f59fb9499ff
Pody <vladpodyganov@yandex.ru>
2023-08-13 16:55:49 +0300

    Test for books

---
+++ LiteraFlowTest/BooksTest.cs
@@ -0,0 +1,42 @@
+﻿namespace LiteraFlowTest;
+
+
+public class BooksTest : BaseTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task CreateEmptyBookTest()
+    {
+        BookModel model = new()
+        {
+            Title = "Test title"
+        };
+        int? bookId = await booksDAL.CreateAsync(model);
+        Assert.That(bookId, Is.GreaterThan(0));
+    }
+
+
+    [Test]
+    public async Task CreateBookWithChaptersTest()
+    {
+        ChapterModel model = new()
+        {
+            Title = "Test title",
+            Text = "Lorem ispum. Lorem ispum! Lorem ispum? Lorem ispum, yes",
+        };
+        int? chapterId = await chaptersDAL.AddAsync(1, model);
+        Assert.That(chapterId, Is.Not.Null);
+    }
+
+
+
+    [Test]
+    public async Task UpdateChapterTest()
+    {
+
+    }
+}
--- LiteraFlowTest/Helpers/BaseTest.cs
+++ LiteraFlowTest/Helpers/BaseTest.cs
@@ -1,7 +1,9 @@
 ﻿using LiteraFlow.Web.BL.Auth;
+using LiteraFlow.Web.BL.Books;
 using LiteraFlow.Web.BL.DBSession;
 using LiteraFlow.Web.BL.WebCookie;
 using LiteraFlow.Web.DAL.Auth;
+using LiteraFlow.Web.DAL.Books;
 using LiteraFlow.Web.DAL.DBSession;
 using LiteraFlow.Web.DAL.UserToken;
 
@@ -10,19 +12,28 @@ namespace LiteraFlowTest.Helpers;
 
 public class BaseTest
 {
-    protected readonly IAuthDAL authDAL = new AuthDAL();
-    protected readonly IAuth auth;
+    //DAL
     protected readonly IDBSessionDAL dBSessionDAL = new DBSessionDAL();
-    protected IDBSession dBSession;
+    protected readonly IUserTokenDAL userTokenDAL = new UserTokenDAL();
+    protected readonly IAuthDAL authDAL = new AuthDAL();
+    protected readonly IChaptersDAL chaptersDAL = new ChaptersDAL();
+    protected readonly IBooksDAL booksDAL = new BooksDAL();
+
     protected IHttpContextAccessor httpContextAccessor = new HttpContextAccessor();
-    protected IUserTokenDAL userTokenDAL = new UserTokenDAL();
-    protected IWebCookie webCookie;
+
+    //BL
+    protected readonly IAuth auth;   
+    protected readonly IDBSession dBSession;   
+    protected readonly IWebCookie webCookie;
+    protected readonly IBooks books;
+
 
     public BaseTest()
     {
         webCookie = new TestCookie();
         dBSession = new DBSession(dBSessionDAL, webCookie);
         auth = new Auth(authDAL, dBSession, userTokenDAL, webCookie);
+        books = new Books(booksDAL, chaptersDAL);
     }
 
 }
