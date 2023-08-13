c5e5a373242d3ff39ff6496276c618149a554226
Pody <vladpodyganov@yandex.ru>
2023-08-13 16:56:57 +0300

    Book and chapter models

---
+++ LiteraFlow/Models/BookModel.cs
@@ -0,0 +1,21 @@
+﻿namespace LiteraFlow.Web.Models;
+
+
+public class BookModel
+{
+    public int BookId { get; set; }
+    public string? Title { get; set; } = null!;
+    public int TypeId { get; set; }
+    public int GenreId { get; set; }
+    public int StatusId { get; set; }
+    public string? Note { get; set; }
+    public string? Description { get; set; }
+    public bool IsAdultContent { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public int WhoCanWatch { get; set; }
+    public int WhoCanDownload { get; set; }
+    public int WhoCanComment { get; set; }
+    public int AmountUnlockedChapters { get; set; }
+    public string? BookImage { get; set; }
+    public double Price { get; set; } = 0.0d;
+}
---
+++ LiteraFlow/Models/ChapterModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LiteraFlow.Web.Models;
+
+public class ChapterModel
+{
+    public int ChapterId { get; set; }
+    public string Title { get; set; } = null!;
+    public string Text { get; set; } = null!;
+    public int BookId { get; set; }
+    public DateTime UpdatedOn { get; set; }
+}
