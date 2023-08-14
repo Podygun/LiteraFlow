969730940fcedbab3a86c45e8d03a83772328666
Pody <vladpodyganov@yandex.ru>
2023-08-14 17:03:41 +0300

    Add IsAuthorized

--- LiteraFlow/BL/Auth/CurrentUser.cs
+++ LiteraFlow/BL/Auth/CurrentUser.cs
@@ -1,57 +1,91 @@
-﻿
-using LiteraFlow.Web.BL.DBSession;
+﻿using LiteraFlow.Web.BL.DBSession;
 using LiteraFlow.Web.BL.Helpers;
 using LiteraFlow.Web.BL.WebCookie;
+using LiteraFlow.Web.DAL.Profiles;
 using LiteraFlow.Web.DAL.UserToken;
 
-namespace LiteraFlow.Web.BL
+namespace LiteraFlow.Web.BL;
+
+
+public class CurrentUser : ICurrentUser
 {
-    public class CurrentUser : ICurrentUser
+    private readonly IDBSession dBSession;
+    private readonly IWebCookie webCookie;
+    private readonly IUserTokenDAL userTokenDAL;
+    private readonly IProfileDAL profileDAL;
+
+    public CurrentUser(
+        IDBSession dBSession,
+        IWebCookie webCookie,
+        IUserTokenDAL userTokenDAL,
+        IProfileDAL profileDAL)
     {
-        private readonly IDBSession dBSession;
-        private readonly IWebCookie webCookie;
-        private readonly IUserTokenDAL userTokenDAL;
+        this.dBSession = dBSession;
+        this.webCookie = webCookie;
+        this.userTokenDAL = userTokenDAL;
+        this.profileDAL = profileDAL;
+    }
 
-        public CurrentUser(
-            IDBSession dBSession,
-            IWebCookie webCookie,
-            IUserTokenDAL userTokenDAL)
+    /// <summary>
+    /// Check for user authenticated
+    /// </summary>
+    public async Task<bool> IsLoggedIn()
+    {
+        //получение id через токен
+        int? userId = await GetUserIdByToken();
+
+        if (userId != null)
         {
-            this.dBSession = dBSession;
-            this.webCookie = webCookie;
-            this.userTokenDAL = userTokenDAL;
+            //TODO Not creating new, but updating LastEnrty time
+            await dBSession.SetUserId((int)userId!);
+            return true;
         }
 
-        public async Task<bool> IsLoggedIn()
+        return await dBSession.IsLoggedIn();
+    }
+
+    /// <summary>
+    /// Check for user have or not profile
+    /// </summary>
+    public async Task<bool> IsAuthorized()
+    {
+        int? userIdByToken = await GetUserIdByToken();
+        int? userIdBySession = await dBSession.GetUserId();
+
+        if (userIdByToken == null && userIdBySession == null) return false;
+
+        if(userIdByToken != null && userIdBySession == null)
         {
-            //получение id через токен
-            int? userId = await GetUserIdByToken();
-
-            if (userId != null)
-            {
-                //TODO Not creating new, but updating LastEnrty time
-                await dBSession.SetUserId((int)userId!);
-                return true;
-            }
-
-            return await dBSession.IsLoggedIn();
+            var profile = await profileDAL.GetAsync((int)userIdByToken);
+            return profile.ProfileId != 0;
+        }
+        else if (userIdByToken == null && userIdBySession != null)
+        {
+            var profile = await profileDAL.GetAsync((int)userIdBySession);
+            return profile.ProfileId != 0;
+        }
+        else if ((userIdByToken != null || userIdBySession != null) && (userIdByToken == userIdBySession))
+        {
+            var profile = await profileDAL.GetAsync((int)userIdByToken);
+            return profile.ProfileId != 0;
         }
 
+        return false;
+    }
 
 
-        public async Task<int?> GetUserIdByToken()
-        {
-            string? token = webCookie.Get(BLConstants.REMEMBER_ME_COOKIE_NAME);
+    public async Task<int?> GetUserIdByToken()
+    {
+        string? token = webCookie.Get(BLConstants.REMEMBER_ME_COOKIE_NAME);
 
-            if (String.IsNullOrEmpty(token)) 
-                return null;
+        if (String.IsNullOrEmpty(token)) 
+            return null;
 
-            Guid? tokenGuid = Helper.StringToGuidOrDefault(token);
+        Guid? tokenGuid = Helper.StringToGuidOrDefault(token);
 
-            if (tokenGuid == null)
-                return null;
+        if (tokenGuid == null)
+            return null;
 
-            return await userTokenDAL.GetUserIdAsync(tokenGuid ?? Guid.Empty);
-        }
+        return await userTokenDAL.GetUserIdAsync(tokenGuid ?? Guid.Empty);
     }
 }
--- LiteraFlow/BL/Auth/ICurrentUser.cs
+++ LiteraFlow/BL/Auth/ICurrentUser.cs
@@ -3,6 +3,7 @@
     public interface ICurrentUser
     {
         public Task<bool> IsLoggedIn();
+        public Task<bool> IsAuthorized();
         public Task<int?> GetUserIdByToken();
 
     }
