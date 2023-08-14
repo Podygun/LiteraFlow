aadca43d2a19b9101f727b4c09835a52628113af
Pody <vladpodyganov@yandex.ru>
2023-08-14 17:04:04 +0300

    ProfileModel

---
+++ LiteraFlow/Models/ProfileModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LiteraFlow.Web.Models;
+
+public class ProfileModel
+{
+    public int ProfileId { get; set; }
+    public int UserId { get; set; }
+    public string? FullName { get; set; }
+    public string? ProfileImage { get; set; }   
+    public string? About { get; set; }
+    public string? Gender { get; set; }
+    public DateTime DateBirth { get; set; }
+}
+
