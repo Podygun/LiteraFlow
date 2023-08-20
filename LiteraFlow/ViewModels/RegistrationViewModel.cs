149340cb4c9b8e0ee1519fe926524c28f017a435
Pody <vladpodyganov@yandex.ru>
2023-08-20 14:37:55 +0300

    UI

--- LiteraFlow/ViewModels/RegistrationViewModel.cs
+++ LiteraFlow/ViewModels/RegistrationViewModel.cs
@@ -8,20 +8,29 @@ public class RegistrationViewModel
     public string? Email { get; set; }
 
     [Required(ErrorMessage = "Введите пароль")]
+    [MinLength(6, ErrorMessage = "Слишком короткий пароль")]
+    [MaxLength(24, ErrorMessage = "Слишком длинный пароль")]
     //[RegularExpression("^.*(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!#$%&? \"]).*$", ErrorMessage ="Пароль слишком простой")]
     public string? Password { get; set; }
 
-    /// <summary>
+   
+    [Required(ErrorMessage = "Подтвердите пароль")]
+    [Compare("Password", ErrorMessage ="Пароли не совпадают")]
+    public string? ConfirmPassword { get; set; }
+    
+
+
+     /// <summary>
     /// Реализовывает IValidatableObject, вызывается при проверке модели IsModelValid
     /// Для реализации доп. проверки свойств модели
     /// </summary>
     /// <returns></returns>
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
     {
-        if (Password == "0000")
+        if (Password == "123456")
         {
             //1 - Сообщение, 2 - Поле, под которым должна появиться ошибка
-            yield return new ValidationResult("Пароль из нулей", new[] { "Password" });
+            yield return new ValidationResult("Не надо так", new[] { "Password" });
         }
     }
 }
