d7e0ac671bcd626db4dbf980c236175ab4783732
Pody <113433675+Podygun@users.noreply.github.com>
2023-08-21 20:09:04 +0300

    fix: rename controller

---
+++ LiteraFlow/Controllers/MyBooksController.cs
@@ -0,0 +1,36 @@
+﻿using LiteraFlow.Web.BL.Books;
+using LiteraFlow.Web.Middleware;
+
+
+namespace LiteraFlow.Web.Controllers;
+
+[SiteAuthenticate]
+public class MyBooksController : Controller
+{
+    private readonly IBooks booksService;
+    private readonly ICurrentUser currentUser;
+
+    public MyBooksController(IBooks books, ICurrentUser currentUser)
+    {
+        this.booksService = books;
+        this.currentUser = currentUser;
+    }
+
+    [HttpGet]
+    [Route("/mybooks")]
+    public async Task<IActionResult> Index()
+    {
+        int? userId = await currentUser.GetCurrentUserId();
+        if (userId == null)
+            throw new Exception("Non authorize user");
+
+        var books = await booksService.GetUserBooks((int)userId);
+
+
+        return View(BookMapper.ModelToViewModel(books).ToList());
+    }
+
+
+
+
+}
