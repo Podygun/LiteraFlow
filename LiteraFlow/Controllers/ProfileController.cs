4118f80aee53ccd1fa08fa7949a2d03f32b03411
Pody <113433675+Podygun@users.noreply.github.com>
2023-08-21 11:01:48 +0300

    feat: exit logic, saveimage

--- LiteraFlow/Controllers/ProfileController.cs
+++ LiteraFlow/Controllers/ProfileController.cs
@@ -1,12 +1,10 @@
-﻿using LiteraFlow.Web.BL.Auth;
-using LiteraFlow.Web.BL.DBSession;
+﻿using LiteraFlow.Web.BL.DBSession;
 using LiteraFlow.Web.BL.Helpers;
 using LiteraFlow.Web.BL.Profiles;
 using LiteraFlow.Web.BL.WebCookie;
 using LiteraFlow.Web.DAL.UserToken;
 using LiteraFlow.Web.Middleware;
 using LiteraFlow.Web.Services;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 namespace LiteraFlow.Web.Controllers;
@@ -17,24 +15,11 @@ public class ProfileController : Controller
 {
     private readonly ICurrentUser currentUser;
     private readonly IProfile profile;
-    //private readonly IWebCookie webCookie;
-    //private readonly IDBSession dBSession;
-    //private readonly IUserTokenDAL userTokenDAL;
 
-
-    public ProfileController(
-        ICurrentUser currentUser, 
-        IProfile profile
-        // IWebCookie webCookie, 
-        // DBSession dBSession, 
-        // IUserTokenDAL userTokenDAL
-        )
+    public ProfileController( ICurrentUser currentUser, IProfile profile)
     {
         this.currentUser = currentUser;
         this.profile = profile;
-        //this.webCookie = webCookie;
-        //this.dBSession = dBSession;
-        //this.userTokenDAL = userTokenDAL;
     }
 
     [HttpGet]
@@ -65,14 +50,7 @@ public class ProfileController : Controller
             throw new Exception("Ошибка обновления данных");
 
         
-        #region ProfileImage Processing
-
-        if (Request.Form.Files.Count > 0)
-            model.ProfileImage = await GetPathOfProcessedImage(Request?.Form?.Files[0]);
-        else
-            model.ProfileImage = null;
-
-        #endregion
+        
 
         model.UserId = profile.UserId;
         await this.profile.CreateOrUpdateAsync(ProfileMapper.ProfileViewModelToModel(model));
@@ -90,27 +68,56 @@ public class ProfileController : Controller
     }
 
     [ActionName("exit")]
-    public async Task<IActionResult> Exit()
+    public async Task<IActionResult> Exit(
+        [FromServices] IWebCookie wc,  
+        [FromServices] IDBSession dbs,
+        [FromServices] IUserTokenDAL ut        
+        )
     {
-        return Redirect("/");
-        //var idSession = webCookie.Get(BLConstants.SESSION_COOKIE_NAME);
-        //Guid? guidSession = Helper.StringToGuidOrDefault(idSession ?? "");
-        //if (guidSession == null) return NotFound();
+        var idSession = wc.Get(BLConstants.SESSION_COOKIE_NAME);
+        Guid? guidSession = Helper.StringToGuidOrDefault(idSession ?? "");
+        if (guidSession == null) 
+            return NotFound();
 
 
         //delete session
-        //var session = await dBSession.GetDBSession();
-        //await dBSession.Delete(session.DbSessionId);
+        var session = await dbs.GetDBSession();
+        await dbs.Delete(session.DbSessionId);
 
-        ////delete all cookies
-        //webCookie.Delete(BLConstants.REMEMBER_ME_COOKIE_NAME);
-        //webCookie.Delete(BLConstants.SESSION_COOKIE_NAME);
+        //delete all cookies
+        wc.Delete(BLConstants.REMEMBER_ME_COOKIE_NAME);
+        wc.Delete(BLConstants.SESSION_COOKIE_NAME);
 
-        ////delete token
-        //var userId = await currentUser.GetCurrentUserId();
-        //if(userId == null) return NotFound();
-        //await userTokenDAL.DeleteAsync((int)userId);
+        //delete token
+        var userId = await currentUser.GetCurrentUserId();
+        if(userId == null) 
+            return NotFound();
+        await ut.DeleteAsync((int)userId);
 
-        //return Redirect("/");
+        return Redirect("/");
+    }
+
+
+    [HttpPost]
+    [Route("/profile/saveimage")]
+    [AutoValidateAntiforgeryToken]
+    public async Task<IActionResult> SaveImage(int? profileId)
+    {
+        if (!ModelState.IsValid)
+            return Redirect("/profile");
+        
+        var profile = await currentUser.GetProfile();
+        if (profile.ProfileId != profileId)
+            throw new Exception("Ошибка обновления данных");
+
+
+        if (Request.Form.Files.Count > 0)
+            profile.ProfileImage = await GetPathOfProcessedImage(Request?.Form?.Files[0]);
+        else
+            profile.ProfileImage = null;
+
+        await this.profile.CreateOrUpdateAsync(profile);
+
+        return Redirect("/profile");
     }
 }
