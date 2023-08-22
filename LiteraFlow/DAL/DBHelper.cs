235707346d5ab5ec35eafec852d4a907378821a7
Pody <113433675+Podygun@users.noreply.github.com>
2023-08-22 11:40:40 +0300

    feat: new QueryCollectionAsync method

--- LiteraFlow/DAL/DBHelper.cs
+++ LiteraFlow/DAL/DBHelper.cs
@@ -1,73 +1,117 @@
-﻿namespace LiteraFlow.Web.DAL
+﻿namespace LiteraFlow.Web.DAL;
+
+
+public static class DBHelper
 {
-    public static class DBHelper
+    public static readonly string ConString = 
+        "Username=postgres;Password=root;Server=localhost;Port=5432;Database=postgres";
+
+    /// <summary>
+    /// Modify DataBase
+    /// </summary>
+    /// <param name="sql">Just Insert, Update, Delete</param>
+    /// <param name="model"></param>
+    /// <returns>Numbers of rows affected</returns>
+    public static async Task<int> ExecuteAsync(string sql, object model)
     {
-        public static readonly string ConString = 
-            "Username=postgres;Password=root;Server=localhost;Port=5432;Database=postgres";
-
-        /// <summary>
-        /// Modify DataBase
-        /// </summary>
-        /// <param name="sql">Just Insert, Update, Delete</param>
-        /// <param name="model"></param>
-        /// <returns>Numbers of rows affected</returns>
-        public static async Task<int> ExecuteAsync(string sql, object model)
+        using (var con = new NpgsqlConnection(ConString))
         {
-            using (var con = new NpgsqlConnection(ConString))
-            {
-                await con.OpenAsync();
-                var result = await con.ExecuteScalarAsync<int>(sql, model);
-                return result;
-            }
+            await con.OpenAsync();
+            var result = await con.ExecuteScalarAsync<int>(sql, model);
+            return result;
         }
+    }
 
-        /// <summary>
-        /// Modify DataBase with "returning"
-        /// </summary>
-        /// <param name="sql">Insert, Update, Delete with "returning"</param>
-        /// <param name="model"></param>
-        /// <returns>Returning value type T</returns>
-        public static async Task<T> ExecuteScalarAsync<T>(string sql, object model)
+    /// <summary>
+    /// Modify DataBase with "returning"
+    /// </summary>
+    /// <param name="sql">Insert, Update, Delete with "returning"</param>
+    /// <param name="model"></param>
+    /// <returns>Returning value type T</returns>
+    public static async Task<T> ExecuteScalarAsync<T>(string sql, object model)
+    {
+        using (var con = new NpgsqlConnection(ConString))
         {
-            using (var con = new NpgsqlConnection(ConString))
-            {
-                await con.OpenAsync();
-                var result = await con.ExecuteScalarAsync<T>(sql, model);
-                return result;
-            }
+            await con.OpenAsync();
+            var result = await con.ExecuteScalarAsync<T>(sql, model);
+            return result;
         }
+    }
 
-        /// <summary>
-        /// Select single row
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="sql"></param>
-        /// <param name="model"></param>
-        /// <returns></returns>
-        public static async Task<T> QueryScalarAsync<T>(string sql, object model)
+    /// <summary>
+    /// Select single row
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="sql"></param>
+    /// <param name="model"></param>
+    /// <returns></returns>
+    public static async Task<T> QueryScalarAsync<T>(string sql, object model)
+    {
+        using (var con = new NpgsqlConnection(ConString))
         {
-            using (var con = new NpgsqlConnection(ConString))
-            {
-                await con.OpenAsync();
-                var result = await con.QueryFirstOrDefaultAsync<T>(sql, model);
-                return result;
-            }
+            await con.OpenAsync();
+            var result = await con.QueryFirstOrDefaultAsync<T>(sql, model);
+            return result;
         }
+    }
 
-        /// <summary>
-        /// Select collection of rows
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="sql"></param>
-        /// <param name="model"></param>
-        /// <returns></returns>
-        public static async Task<IList<T>> QueryCollectionAsync<T>(string sql, object model)
+    /// <summary>
+    /// Select collection of rows with params
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="sql"></param>
+    /// <param name="model"></param>
+    /// <returns></returns>
+    public static async Task<IList<T>> QueryCollectionAsync<T>(string sql, object model)
+    {
+        using (var con = new NpgsqlConnection(ConString))
         {
-            using (var con = new NpgsqlConnection(ConString))
+            await con.OpenAsync();
+            var result = await con.QueryAsync<T>(sql, model);
+            return result.ToList();
+        }
+    }
+    /// <summary>
+    /// Select collection of rows
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static async Task<List<T>> QueryCollectionAsync<T>(string sql)
+    {
+        using (var con = new NpgsqlConnection(ConString))
+        {
+            await con.OpenAsync();
+            var result = await con.QueryAsync<T>(sql);
+            return result.ToList();
+        }
+    }
+
+
+
+    public static async Task<int?> AddBookWithTransactionAsync(string sqlInsertBook, BookModel book, string sqlInsertAuthorBook, int profileId)
+    {
+        using (var con = new NpgsqlConnection(ConString))
+        {
+            await con.OpenAsync();
+            using(var transaction = con.BeginTransaction())
             {
-                await con.OpenAsync();
-                var result = await con.QueryAsync<T>(sql, model);
-                return result.ToList();
+                try
+                {
+                    //add book
+                    int bookId = await ExecuteScalarAsync<int>(sqlInsertBook, book);
+
+                    //add many-to-many relation to AuthorsBooks Table
+                    await ExecuteAsync(sqlInsertAuthorBook, new { bookid = bookId, profileid = profileId });
+
+                    transaction.Commit();
+
+                    return bookId;
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    return null;
+                }
             }
         }
     }
