d7c49616eedc300f8035b5a0e9041fe56eb61be9
Pody <113433675+Podygun@users.noreply.github.com>
2023-08-22 11:41:59 +0300

    Create NewBookController.cs

---
+++ LiteraFlow/Controllers/NewBookController.cs
@@ -0,0 +1,54 @@
+﻿using LiteraFlow.Web.Middleware;
+
+namespace LiteraFlow.Web.Controllers
+{
+
+    [Controller]
+    [SiteAuthenticate]
+    public class NewBookController : Controller
+    {
+        [HttpGet]
+        [Route("/newbook/basic")]
+        public IActionResult Basic(BookViewModel book)
+        {
+            return (View("Index", book));
+        }
+
+        [HttpPost]
+        [Route("/newbook/settings")]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Settings(BookViewModel book)
+        {
+            var foo = book;
+            return View("Privacy");
+        }
+
+
+
+        [HttpPost]
+        [Route("/newbook/save")]
+        public IActionResult CreateBook(BookViewModel book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Redirect("/mybooks");
+            }
+            return Redirect("/mybooks");
+        }
+
+        [HttpGet]
+        [Route("/newbook/notfoo")]
+        public IActionResult foo()
+        {
+            return View("Privacy");
+        }
+
+        //[HttpPost]
+        //[Route("/mybook/chapter")]
+        //public IActionResult PostChapter(ChapterViewModel chapter)
+        //{
+        //    return View();
+        //}
+
+    }
+}
