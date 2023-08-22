bbb57a065770617eb29769da63038a4accf738a3
Pody <113433675+Podygun@users.noreply.github.com>
2023-08-22 11:41:54 +0300

    feat: books relations dal

---
+++ LiteraFlow/DAL/BooksRelaltions/BooksRelationDAL.cs
@@ -0,0 +1,19 @@
+﻿namespace LiteraFlow.Web.DAL.BooksRelaltions;
+
+public class BooksRelationDAL
+{
+    public async Task<List<BookTypeModel>> GetBookTypes() =>
+        await DBHelper.QueryCollectionAsync<BookTypeModel>("select * from BookTypes");
+
+    public async Task<List<BookGenreModel>> GetBookGenres() =>
+        await DBHelper.QueryCollectionAsync<BookGenreModel>("select * from BookGenres");
+
+    public async Task<List<TagModel>> GetBookTags() =>
+        await DBHelper.QueryCollectionAsync<TagModel>("select * from Tags");
+
+    public async Task<List<BookStatusModel>> GetBookStatuses() =>
+        await DBHelper.QueryCollectionAsync<BookStatusModel>("select * from BookStatuses");
+
+    public async Task<List<PermissionModel>> GetPermissions() =>
+        await DBHelper.QueryCollectionAsync<PermissionModel>("select * from PermissionTypes");
+}
