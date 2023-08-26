7016905d20c9699a897b5035e5cebce2f1a0828b
Pody <113433675+Podygun@users.noreply.github.com>
2023-08-26 12:37:46 +0300

    init: UserAuthorized attribute
    Checks for exists profile on current logged in user

---
+++ LiteraFlow/Middleware/UserAuthorized.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace LiteraFlow.Web.Middleware;
+
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+public class UserAuthorized : Attribute, IAsyncAuthorizationFilter
+{
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        ICurrentUser? currentUser = context.HttpContext.RequestServices.GetService<ICurrentUser>();
+        if (currentUser == null)
+            throw new Exception("No user middleware");
+        
+        bool isLoggedIn = await currentUser.IsLoggedIn();
+        if (!isLoggedIn)
+        {
+            context.Result = new RedirectResult("/Login");
+            return;
+        }
+
+        var profile = await currentUser.GetProfile();
+
+        if (profile.ProfileId == null || profile.ProfileId == 0)
+        {
+            context.Result = new RedirectResult("/profile");
+            return;
+        }
+
+
+    }
+}
