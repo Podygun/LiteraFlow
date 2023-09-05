945a4f4df93d88667a7d2e98619291ffa3a9eea5
Pody <113433675+Podygun@users.noreply.github.com>
2023-09-05 21:45:20 +0300

    Update CacheConstants.cs

--- LiteraFlow/Services/CacheConstants.cs
+++ LiteraFlow/Services/CacheConstants.cs
@@ -5,5 +5,6 @@
         public const string USER_BOOKS = "user_books";
         public const string PROFILE = "profile";
         public const string PROFILE_ID = "profile_id";
+        public const string BOOK_CHAPTERS = "book_chapters";
     }
 }
